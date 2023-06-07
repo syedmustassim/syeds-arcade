4c87e60d018703df25671ac8b2ac5dc551969b65
Syed Mustassim <smustassim@gmail.com>
2023-06-07 15:16:11 +0530

    final toast added

--- src/Components/OrderDetails.js
+++ src/Components/OrderDetails.js
@@ -1,41 +1,54 @@
-import { useCart } from "../Context/CartContext"
+import { toast } from "react-hot-toast";
+import { useCart } from "../Context/CartContext";
+import { useNavigate } from "react-router-dom";
 
 export const OrderDetails = () => {
+  const navigate = useNavigate();
 
-    const {cartState: {cart}, totalPrice} = useCart();
+  const placeOrder = () => {
+    toast.success("Order placed!");
+    navigate("/");
+  };
 
-    return(
-        <div className="order-details-container">
-            <div className="order-details-heading">
-                <h1>Order Details</h1>
-            </div>
-            <hr/>
-            <div className="order-details-h3">
-                <h3>Games:</h3>
-                {
-                    cart?.map((item) => {
-                        const {title, qty} = item;
-                        return(
-                            <ul>
-                                <li className="order-details-list">{title} - Quantity: {qty}</li>
-                            </ul>
-                        )
-                    })
-                }
-            </div>
-            <hr/>
-            <div className="order-price-content">
-            <h1>Price Details</h1>
-            {
-                cart?.map((item) => {
-                    const {title, price} = item;
-                    return(<p>{title} - &#8377;{price}</p>)
-                })
-            }
-            <h2>Shipping and delivery charges: Nil. On the house.</h2>
-            <h2>Total amount: &#8377; {totalPrice}</h2>
-            </div>
-            <button> Place Order </button>
-        </div>
-    )
-}
+  const {
+    cartState: { cart },
+    totalPrice,
+  } = useCart();
+
+  return (
+    <div className="order-details-container">
+      <div className="order-details-heading">
+        <h1>Order Details</h1>
+      </div>
+      <hr />
+      <div className="order-details-h3">
+        <h3>Games:</h3>
+        {cart?.map((item) => {
+          const { title, qty } = item;
+          return (
+            <ul>
+              <li className="order-details-list">
+                {title} - Quantity: {qty}
+              </li>
+            </ul>
+          );
+        })}
+      </div>
+      <hr />
+      <div className="order-price-content">
+        <h1>Price Details</h1>
+        {cart?.map((item) => {
+          const { title, price } = item;
+          return (
+            <p>
+              {title} - &#8377;{price}
+            </p>
+          );
+        })}
+        <h2>Shipping and delivery charges: Nil. On the house.</h2>
+        <h2>Total amount: &#8377; {totalPrice}</h2>
+      </div>
+      <button onClick={placeOrder}> Place Order </button>
+    </div>
+  );
+};
