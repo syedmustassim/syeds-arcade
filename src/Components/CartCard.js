ef66f0426a578ee429bdefbad3e2160228713112
Syed Mustassim <smustassim@gmail.com>
2023-06-07 13:19:08 +0530

    updated INR to rupee symbol

--- src/Components/CartCard.js
+++ src/Components/CartCard.js
@@ -1,32 +1,54 @@
 import { useCart } from "../Context/CartContext";
 import { useWishlist } from "../Context/WishlistContext";
 
-import '../Pages/Cart.css'
+import "../Pages/Cart.css";
 
-const CartCard = ({cartProduct}) => {
+const CartCard = ({ cartProduct }) => {
+  const { _id, title, price, qty, image_link } = cartProduct;
+  const { removeFromCart, updateCartQuantity } = useCart();
+  const { addToWishlist } = useWishlist();
 
-    const {_id, title, price, qty, image_link} = cartProduct;
-    const {removeFromCart, updateCartQuantity} = useCart();
-    const {addToWishlist} = useWishlist();
-
-    return(
-        <div>   
-            <div className="cart-card">
-                <img src={image_link} alt="poster" />
-                <div className="card-details">
-                    <h3 className="cart-title">{title}</h3>
-                    <h3>Price: INR {price}</h3>
-                    <div>
-                    <button className="quantity-minus" onClick={() => updateCartQuantity(cartProduct, "decrement")} disabled={qty <=1 }>-</button>Quantity: {qty}<button className="quantity-plus" onClick={() => updateCartQuantity(cartProduct, "increment")}>+</button>
-                    </div>
-                    <div className="cart-card-btn">
-                        <button className="cart-remove-btn" onClick={() => removeFromCart(cartProduct)}>Remove from cart</button>
-                        <button className="cart-wishlist-btn" onClick={() => addToWishlist(cartProduct)}>Add to Wishlist</button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="cart-card">
+        <img src={image_link} alt="poster" />
+        <div className="card-details">
+          <h3 className="cart-title">{title}</h3>
+          <h3>Price: &#8377; {price}</h3>
+          <div>
+            <button
+              className="quantity-minus"
+              onClick={() => updateCartQuantity(cartProduct, "decrement")}
+              disabled={qty <= 1}
+            >
+              -
+            </button>
+            Quantity: {qty}
+            <button
+              className="quantity-plus"
+              onClick={() => updateCartQuantity(cartProduct, "increment")}
+            >
+              +
+            </button>
+          </div>
+          <div className="cart-card-btn">
+            <button
+              className="cart-remove-btn"
+              onClick={() => removeFromCart(cartProduct)}
+            >
+              Remove from cart
+            </button>
+            <button
+              className="cart-wishlist-btn"
+              onClick={() => addToWishlist(cartProduct)}
+            >
+              Add to Wishlist
+            </button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default CartCard;
--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,55 +1,82 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import { useFilterContext } from "../Context/FilterContext";
-import './ProductList.css'
+import "./ProductList.css";
 import { useAuthContext } from "../Context/AuthContext";
 import { useCart } from "../Context/CartContext";
 import { useWishlist } from "../Context/WishlistContext";
 
+const ProductList = ({ filterProducts }) => {
+  // const {filterProducts} = useFilterContext();
 
-const ProductList = ({filterProducts}) =>{
+  const navigate = useNavigate();
+  // const goTo = () =>{
+  //     navigate("/cart")
+  // }
 
-    // const {filterProducts} = useFilterContext();
-    
-    const navigate = useNavigate()
-    // const goTo = () =>{
-    //     navigate("/cart")
-    // }
+  const { token } = useAuthContext();
+  const { addToCart, findItemInCart } = useCart();
+  const { addToWishlist } = useWishlist();
 
-    const {token} = useAuthContext();
-    const {addToCart, findItemInCart} = useCart();
-    const {addToWishlist} = useWishlist();
-
-    return(
-        <div className="flex-container">
-            {   filterProducts.length === 0 ? <div> <h1> No products found </h1> </div> :
-                filterProducts.map((item) => {
-                    const {id,image_link,title,price,year,rating,categoryName} = item;
-                    return(
-                        <div className="layout" key={id}>
-                            <div className="card">
-                                <img src={image_link} alt={title} className="card-img"/>
-                            <div className="card-body">
-                             
-                                <NavLink to={`/products/${id}`} className="card-body-link" style={{color: "white"}}>
-                                <h1 className="card-heading">{title}</h1>
-                                <h2 className="card-sub-heading">Price: INR {price}</h2>
-                                <h2 className="card-info">Year: {year}</h2>
-                                <h2 className="card-info">Rating: {rating}</h2>
-                                <h2 className="card-info">Genre: {categoryName}</h2>
-                                </NavLink>
-                                
-                                <div className="btn-container">
-                                    <button className="btn-card" onClick={() => token ? findItemInCart(item._id) ? navigate('/cart') : addToCart(item)  : navigate('/login')} > {token && findItemInCart(item._id) ? "Go to cart" : "Add to Cart"}</button>
-                                    <button className="btn-card" onClick={() => addToWishlist(item)}> Add to Wishlist</button>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                    )
-                })
-            }
+  return (
+    <div className="flex-container">
+      {filterProducts.length === 0 ? (
+        <div>
+          {" "}
+          <h1> No products found </h1>{" "}
         </div>
-    )
-}
+      ) : (
+        filterProducts.map((item) => {
+          const { id, image_link, title, price, year, rating, categoryName } =
+            item;
+          return (
+            <div className="layout" key={id}>
+              <div className="card">
+                <img src={image_link} alt={title} className="card-img" />
+                <div className="card-body">
+                  <NavLink
+                    to={`/products/${id}`}
+                    className="card-body-link"
+                    style={{ color: "white" }}
+                  >
+                    <h1 className="card-heading">{title}</h1>
+                    <h2 className="card-sub-heading">Price: &#8377; {price}</h2>
+                    <h2 className="card-info">Year: {year}</h2>
+                    <h2 className="card-info">Rating: {rating}</h2>
+                    <h2 className="card-info">Genre: {categoryName}</h2>
+                  </NavLink>
+
+                  <div className="btn-container">
+                    <button
+                      className="btn-card"
+                      onClick={() =>
+                        token
+                          ? findItemInCart(item._id)
+                            ? navigate("/cart")
+                            : addToCart(item)
+                          : navigate("/login")
+                      }
+                    >
+                      {" "}
+                      {token && findItemInCart(item._id)
+                        ? "Go to cart"
+                        : "Add to Cart"}
+                    </button>
+                    <button
+                      className="btn-card"
+                      onClick={() => addToWishlist(item)}
+                    >
+                      {" "}
+                      Add to Wishlist
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
+};
 
 export default ProductList;
